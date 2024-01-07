35ceefb5c883fa8bf5865def1232bf3d3eeeb297
hyeonseok <hs@hyeonseog-ui-iMac.local>
2024-01-08 00:51:34 +0900

    컨트랙트 디버깅

--- contracts/DecentralBank.sol
+++ contracts/DecentralBank.sol
@@ -1,8 +1,18 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity >=0.8.0 <0.8.18;
 
+import './RWD.sol';
+import './Tether.sol';
+
 contract DecentralBank {
     string public  name = 'Decentral Bank';
     address public owner;
+    Tether public tether;
+    RWD public rwd;
+    
+
+    constructor(RWD _rwd, Tether _tether) {
+
+    }
 
 }
